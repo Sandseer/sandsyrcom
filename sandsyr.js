53f790b2b4e11a22eba8ed4c72c208ce801fc54f
SandSyR <85406714+SandSyR@users.noreply.github.com>
2022-04-21 21:12:53 -0400

    Update sandsyr.js

---
+++ sandsyr.js
@@ -0,0 +1,15 @@
+console.log("Yo mama fat");
+
+// The following code is from our lord and savior w3schools
+
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
+
+// w3schools code end
