aee5a83d08f6498ce4355101db335ec384f736e7
SandSyR <85406714+SandSyR@users.noreply.github.com>
2022-07-24 21:25:25 -0400

    Update sandsyr.js

--- sandsyr.js
+++ sandsyr.js
@@ -13,3 +13,29 @@ function myFunction() {
 }
 
 // w3schools code end
+
+const classist = document.getElementsByClassName("tableThingy");
+
+function borderTwitch() {
+  var twitch = document.getElementById("twitch");
+  classist.classList.add("borderHid");
+  twitch.classList.remove("borderHid");
+}
+
+function borderYoutube() {
+  var youtube = document.getElementById("youtube");
+  classist.classList.add("borderHid");
+  youtube.classList.remove("borderHid");
+}
+
+function borderDiscord() {
+  var discord = document.getElementById("discord");
+  classist.classList.add("borderHid");
+  discord.classList.remove("borderHid");
+}
+
+function borderTiktok() {
+  var tiktok = document.getElementById("tiktok");
+  classist.classList.add("borderHid");
+  tiktok.classList.remove("borderHid");
+}
